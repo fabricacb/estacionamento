2093c4123531d7a6bbf02115424a563866bb3150
alefe patrick <alefepdias@gmail.com>
2021-02-06 15:40:24 -0400

    Retirando o flayway do pom e apagando os scripts do flyway adicionando um arquivo de texto para guardar os scripts

--- src/main/resources/db/migration/V01__criar_banco.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE 
-	`opoyk3y53ix9wwkm`.`funcionario` (
-		id_funcionario int PRIMARY KEY AUTO_INCREMENT,
-		senha varchar(200),
-		nome varchar(200),
-		email varchar(100)
-	)
-ENGINE=InnoDB 
-DEFAULT CHARSET=utf8;
--- src/main/resources/db/migration/V02__alterar_tipo_id.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE funcionario MODIFY id_funcionario long;
