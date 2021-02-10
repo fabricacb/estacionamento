b07ffa9230c057cbee82d9f59ca3faa6699e3438
alefe patrick <alefepdias@gmail.com>
2021-02-10 02:01:52 -0400

    Reeformando o projeto

--- src/main/java/br/com/sistemaestacionamento/service/FuncionarioService.java
+++ src/main/java/br/com/sistemaestacionamento/service/FuncionarioService.java
@@ -1,48 +1,12 @@
 package br.com.sistemaestacionamento.service;
 
-import java.net.URI;
-import java.util.List;
+import br.com.sistemaestacionamento.model.entity.Funcionario;
 
-import javax.annotation.ManagedBean;
-import javax.servlet.http.HttpServletResponse;
+public interface FuncionarioService {
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import br.com.sistemaestacionamento.model.Funcionario;
-import br.com.sistemaestacionamento.repository.FuncionarioRepository;
-
-@ManagedBean
-public class FuncionarioService {
+	Funcionario salvarFuncionario(String email, String senha);
 	
-	@Autowired
-	private FuncionarioRepository funcionarioRepository;
-
-	public ResponseEntity<Funcionario> criar(@RequestBody Funcionario funcionario, HttpServletResponse response) {
-		Funcionario funcionarioSalvo = funcionarioRepository.save(funcionario);
-		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-				.buildAndExpand(funcionarioSalvo.getId()).toUri();
-		response.setHeader("location", uri.toASCIIString());
-		
-		return ResponseEntity.created(uri).body(funcionarioSalvo);
-	}
-	
-	@Autowired
-	public FuncionarioService(FuncionarioRepository funcionarioRepository) {
-		this.funcionarioRepository = funcionarioRepository;
-	}
+	void validarEmail(String email);
 	
 	
-	public List<Funcionario> listar(){
-		return funcionarioRepository.findAll();
-	}
-	
-	public Funcionario buscar(Long id) {
-		return funcionarioRepository
-				.findById(id)
-				.orElse(null);
-	}
 }
