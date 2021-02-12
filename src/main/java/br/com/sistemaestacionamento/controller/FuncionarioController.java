e3cfa461e72000814af162c682a89b95ad9cd635
Willian Maltus <willianmaltus@bol.com.br>
2021-02-12 16:07:12 -0300

    Implementado o novo metodo post do funcionario

--- src/main/java/br/com/sistemaestacionamento/controller/FuncionarioController.java
+++ src/main/java/br/com/sistemaestacionamento/controller/FuncionarioController.java
@@ -1,13 +1,17 @@
 package br.com.sistemaestacionamento.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.sistemaestacionamento.api.dto.FuncionarioDTO;
+import br.com.sistemaestacionamento.exception.RegraNegocioException;
 import br.com.sistemaestacionamento.model.entity.Funcionario;
+import br.com.sistemaestacionamento.model.entity.Funcionario.FuncionarioBuilder;
 import br.com.sistemaestacionamento.service.FuncionarioService;
 
 @RestController
@@ -20,12 +24,21 @@ public class FuncionarioController {
 		this.service = service;
 	}
 
+	@PostMapping
 	public ResponseEntity salvarFuncionario(@RequestBody FuncionarioDTO dto) {
 		
 		Funcionario funcionario = Funcionario.builder()
 				.nome(dto.getNome())
 				.email(dto.getEmail())
-				.senha(dto.getSenha());
+				.senha(dto.getSenha())
+				.build();
+		
+		try {
+			Funcionario funcionarioSalvo = service.salvarFuncionario(funcionario);
+			return new ResponseEntity(funcionarioSalvo, HttpStatus.ACCEPTED);	
+		} catch (RegraNegocioException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
 				
 	}
 	
